1d4ce92d3366bc76a679c7d7c6491fc7aa718c52
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-06-24 20:47:35 -0700

    update

--- networkMonitor.js
+++ networkMonitor.js
@@ -1,6 +1,8 @@
-const arp = require('node-arp');
+const ping = require('net-ping');
 const os = require('os');
+const arp = require('node-arp');
 
+const session = ping.createSession();
 let knownDevices = new Map();
 const interval = 60000; // 1 minute interval
 
@@ -9,7 +11,6 @@ function getLocalIP() {
   for (const iface of Object.values(ifaces)) {
     for (const alias of iface) {
       if (alias.family === 'IPv4' && !alias.internal && !alias.mac.startsWith('02:')) {
-        // Exclude virtual interfaces and WiFi interfaces (common prefix for WiFi MAC addresses)
         return alias.address;
       }
     }
@@ -26,27 +27,55 @@ function getNetworkRange() {
   return range;
 }
 
+function getDeviceName(ip, callback) {
+  // Use arp to get the MAC address
+  arp.getMAC(ip, (err, mac) => {
+    if (err) {
+      callback(null);
+    } else {
+      callback(mac);
+    }
+  });
+}
+
 function scanNetwork(callback) {
   const networkRange = getNetworkRange();
   const newDevices = [];
+  let completedRequests = 0;
 
   networkRange.forEach(ip => {
-    arp.getMAC(ip, (err, mac) => {
-      if (!err && mac) {
-        if (!knownDevices.has(mac)) {
-          knownDevices.set(mac, { ip, mac });
-          newDevices.push({ ip, mac });
-        } else {
-          knownDevices.set(mac, { ip, mac });
+    session.pingHost(ip, (error, target) => {
+      completedRequests++;
+      if (!error) {
+        getDeviceName(target, (mac) => {
+          const device = { ip: target, mac: mac, name: null }; // Placeholder for name
+          if (!knownDevices.has(target)) {
+            knownDevices.set(target, device);
+            newDevices.push(device);
+          } else {
+            knownDevices.set(target, device);
+          }
+          // When all requests are completed, call the callback
+          if (completedRequests === networkRange.length) {
+            callback(newDevices, Array.from(knownDevices.values()));
+          }
+        });
+      } else {
+        if (completedRequests === networkRange.length) {
+          callback(newDevices, Array.from(knownDevices.values()));
         }
       }
     });
   });
-
-  callback(newDevices, Array.from(knownDevices.values()));
 }
 
 function startMonitoring(callback) {
+  // Initial scan
+  scanNetwork((newDevices, allDevices) => {
+    callback(newDevices, allDevices);
+  });
+
+  // Periodic scan
   setInterval(() => {
     scanNetwork((newDevices, allDevices) => {
       callback(newDevices, allDevices);
